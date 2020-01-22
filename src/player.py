3c15db76eaecd293d54bffedaa026f4464700ee2
GregJordanSr <gregljordan@gmail.com>
2020-01-21 22:44:03 -0500

    Made a player class

--- src/player.py
+++ src/player.py
@@ -1,7 +1,10 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
 class Player():
-    def __init__(self, starting_room):
-        self.starting_room = starting_room
+    def __init__(self, current_room):
+        self.current_room = starting_room
+    def change_location(self, current_location):
+        self.current_location = current_location
+    def __str__(self):
+        return f"Player is in the {self.current_room} room"
 
-        
